e84baced5454eb830f900f147be980fd33e5ebad
Jack Andrews <45759583+jwandrews99@users.noreply.github.com>
2020-03-31 16:23:44 +0100

    build folder

---
+++ client/build/precache-manifest.7224e3ffe8f2510e77dffaa8f6100ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9ebceda3962f6dea8e07c83e810557",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9540472dadff047cba87",
+    "url": "/static/css/main.0fba9f99.chunk.css"
+  },
+  {
+    "revision": "233292086317f554992a",
+    "url": "/static/js/2.1d486d5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d486d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9540472dadff047cba87",
+    "url": "/static/js/main.f59e412f.chunk.js"
+  },
+  {
+    "revision": "3e5e20c52f8e9bacf785",
+    "url": "/static/js/runtime-main.b6a55b0e.js"
+  }
+]);
